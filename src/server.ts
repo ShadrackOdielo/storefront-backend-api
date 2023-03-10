14a3e57efde67ba58b5474b7dce890e3d9260d39
ShadrackOdielo <shadrackodielo@gmail.com>
2023-03-10 09:23:36 +0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,33 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, {Application, Request, Response} from "express"
+import bodyParser from "body-parser"
+import path from "path"
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+import userRoutes from "./handlers/user"
+import productRoutes from "./handlers/product"
+import orderRoutes from "./handlers/orders"
+
+const app: Application = express()
+
+let port: number = 3000
+
+if (process.env.ENV === "test") {
+  port = 3001
+}
+
+const address: string = `127.0.0.1:${port}`
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../index.html"))
 })
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+userRoutes(app)
+productRoutes(app)
+orderRoutes(app)
+
+app.listen(port, () => {
+  console.info(`Starting app on: ${address}`)
 })
+
+export default app
